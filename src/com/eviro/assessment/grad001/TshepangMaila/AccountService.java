345a02f4b5be29ccf1458eb18d4dc3945842b40c
MaddoxMaila <37212622+MaddoxMaila@users.noreply.github.com>
2021-04-15 20:02:37 +0200

    Project Started!, Implemented The AccountService Interface

---
+++ src/com/eviro/assessment/grad001/TshepangMaila/AccountService.java
@@ -0,0 +1,21 @@
+package com.eviro.assessment.grad001.TshepangMaila;
+
+import java.math.BigDecimal;
+
+/**
+ * AccountService Interface includes one method to be implemented By
+ * CurrentAccount & SavingsAccount classes
+ * @author Tshepang Maddox Maila
+ * @since 15 April 2021
+ *
+ * */
+
+public interface AccountService {
+
+    /**
+     * @param accountNum : Account Number Associated With A User
+     * @param amountToWitdraw : Amount Of Money To Be Withdrawn By User
+     *
+     * */
+    public void withdraw(String accountNum, BigDecimal amountToWitdraw);
+}
